eb3e018c01ea6677fc41fd1783c566b731f7f7cb
lotiguard_02 <lotiguard@gmail.com>
2015-10-02 13:57:20 +0300

    CRUD

--- js/script.js
+++ js/script.js
@@ -13,9 +13,11 @@ angular.module('appBookmarks', [])
 			{"id": 2, "title": "Behance", "url": "https://www.behance.net/", "category": "design"},
 			{"id": 3, "title": "Deviantart", "url": "http://www.deviantart.com/", "category": "design"},
 			{"id": 4, "title": "Bootstrap", "url": "http://getbootstrap.com/components/#alerts", "category": "exercise"},
-			{"id": 4, "title": "CodePen", "url": "http://codepen.io/", "category": "useful"},
-			{"id": 5, "title": "Randomuser", "url": "https://randomuser.me/", "category": "useful"},
+			{"id": 5, "title": "CodePen", "url": "http://codepen.io/", "category": "useful"},
+			{"id": 6, "title": "Randomuser", "url": "https://randomuser.me/", "category": "useful"}
 		];
+
+		$scope.newBookmark = null;
 
 		$scope.currentCategory = null;
 
@@ -27,11 +29,60 @@ angular.module('appBookmarks', [])
 		};
 
 		function isCurrentCategory( category ){
-			return category !== null && category=== $scope.currentCategory;
+			//return category !== null && category=== $scope.currentCategory;
+			return $scope.currentCategory !== null && category.name === $scope.currentCategory.name;
 		}
 
 		$scope.setCurrentCategory = setCurrentCategory;
 		$scope.isCurrentCategory = isCurrentCategory;
+
+		// CRUD
+		
+		function resetCreateForm(){
+			$scope.newBookmark = {
+				'title': '',
+				'url': '',
+				'category': $scope.currentCategory
+			};
+
+		};
+
+		function createBookmark(bookmark){
+
+			$scope.newBookmark = angular.copy(bookmark);
+			$scope.newBookmark.id = $scope.bookmarks.length;
+			$scope.newBookmark.category = $scope.currentCategory.name;
+			console.log($scope.newBookmark);
+			$scope.bookmarks.push($scope.newBookmark);
+
+			resetCreateForm();
+		};
+
+		$scope.createBookmark = createBookmark;
+
+		$scope.editBookmark = null;
+
+		function setEditingBookmark(bookmark) {
+			$scope.editBookmark = angular.copy(bookmark);
+		}
+
+		function updateBookmark(bookmark){
+			var index = _.findIndex($scope.bookmarks, function(b){
+				return b.id == bookmark.id;
+			} );
+
+			$scope.bookmarks[index] = bookmark;
+			$scope.editBookmark = null;
+			$scope.isEditing = false;
+		}
+
+		function isSelectedBookmark(bookmarkId){
+			return $scope.editBookmark !== null && $scope.editBookmark.id === bookmarkId;
+		}
+
+		$scope.isSelectedBookmark = isSelectedBookmark;
+		$scope.setEditingBookmark = setEditingBookmark;
+		$scope.updateBookmark = updateBookmark;
 
 		// CREATING AND EDITING
 		
